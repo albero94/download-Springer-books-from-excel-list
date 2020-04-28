6d30ba43fa280f7e9fb199cf498cc90f833bda18
albero94 <ealbero94@gmail.com>
2020-04-28 18:33:38 -0400

    chore: remove extra library

--- main.js
+++ main.js
@@ -1,44 +1,62 @@
 const fetch = require("node-fetch");
-const request = require('request');
 const fs = require("fs");
-const readXlsxFile = require('read-excel-file/node');
+const readXlsxFile = require("read-excel-file/node");
 
 // Define save path and excel file with books
-const path = './../Books/'
-const inputFile = './../Springer_Ebooks_short.xlsx'
+const PATH = "./../Books/";
+const INPUT_FILE = "./../Springer_Ebooks_short.xlsx";
 
-readXlsxFile(inputFile).then((rows) => {
-    rows.forEach(row => {
-        const redirectUrl = row[4];
-        request(redirectUrl, (e, res) =>{
-            let webUrl = res.request.uri.href;
-            let pdfUrl = 'https://link.springer.com/content/pdf' + webUrl.substring(webUrl.lastIndexOf('/'))
-            const name = row[1];
+downloadPdfFromExcel();
 
-            downloadFile(pdfUrl, (path + name + '.pdf'));
-        })
 
+function downloadPdfFromExcel(){
+  readXlsxFile(INPUT_FILE).then((rows) => {
+    rows.forEach(async (row) => {
+        let {name , pdfUrl} = await readRow(row);
+
+        name = removeSpecialCharacters(name);
+
+        downloadFile(pdfUrl, PATH + name + ".pdf");
     });
-})
+  });
+}
 
+function removeSpecialCharacters(name){
+  return name.replace(/[^a-zA-Z ]/g, " ");
+}
+
+async function readRow(row) {
+  const redirectUrl = row[4];
+  const name = row[1];
+  
+  const res = await fetch(redirectUrl);
+
+  let webUrl = res.url;
+  let pdfUrl =
+    "https://link.springer.com/content/pdf" +
+    webUrl.substring(webUrl.lastIndexOf("/"));
+
+  return { name, pdfUrl };
+}
 
 async function downloadFile(url, path) {
   const res = await fetch(url);
-  try {
     const fileStream = fs.createWriteStream(path);
+    
     await new Promise((resolve, reject) => {
-      console.log('Starting download of ' + path);
+      console.log("Starting download of " + path);
+
       res.body.pipe(fileStream);
+
       res.body.on("error", (err) => {
         reject(err);
         console.log(err);
       });
+
       fileStream.on("finish", function () {
-        console.log('Finishing download of ' + path);
+        console.log("Finishing download of " + path);
         resolve();
       });
+
     });
-  } catch (ex) {
-    console.log(ex);
-  }
 }
